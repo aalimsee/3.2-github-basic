0c3ea8ca75acd46a6dd5c674fdaf3a16b25ab3cb
aaron lim <aaron.limse@hotmail.com>
2025-04-02 21:34:16 +0800

    Adding new s3 bucket

--- main.tf
+++ main.tf
@@ -8,13 +8,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.9" # Adjust as needed
+      version = "~> 5.0" # Adjust as needed
     }
   }
-  required_version = "~> 1.2.0" # Use the latest stable version or specify your desired version
+  required_version = ">= 1.0.0" # Use the latest stable version or specify your desired version
 }
-
-
 
 
 
